d2cd1c83492199ae9d69d45851f0f4c6ab8ea374
tatiAndonegui <115169557+tatiAndonegui@users.noreply.github.com>
2024-05-13 20:57:13 -0300

    creacion de la clase transporte

---
+++ clases/transporte.py
@@ -0,0 +1,43 @@
+from abc import ABC, abstractmethod
+
+class Transporte(ABC):
+    def __init__(self,color, marca, aceleracion, velocidad):
+        self.color=color
+        self.marca=marca
+        self.aceleracion=aceleracion
+        self.velocidad=velocidad
+
+
+    #getters y setters--CORREGIR COMO EN VEHICULO
+    
+    def set_color(self,color):
+        self.color=color
+    def set_marca(self,marca):
+        self.marca = marca
+    def set_aceleracion(self,aceleracion):
+        self.aceleracion = aceleracion
+    def set_velocidad(self,velocidad):
+        self.velocidad =velocidad
+
+    def get_color(self):
+        return self.color
+    def get_marca(self):
+        return self.marca
+    def get_aceleracion(self):
+        return self.aceleracion
+    def get_velocidad(self):
+        return self.velocidad
+
+#Metodos Abstractos   
+    @abstractmethod 
+    def volar(self):
+        pass
+    def conducir(self):
+        pass 
+
+#Metodos
+    def acelera(self):
+        pass
+    
+    def frena(self):
+        pass
