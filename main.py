e117837fb0ca609a4f6d28c2bc0b92f0335609e7
tatiAndonegui <tatiana.andonegui@davinci.edu.ar>
2024-05-14 22:51:51 -0300

    base de datos

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from clases.vehiculo import Vehiculo
 from clases.automovil import Automovil
 from clases.automovilVolador import AutomovilVolador
 from modulo.operacion import operacion
+import sqlite3
 
 
 
 #creacion de un automovil    
-coche=Automovil("amarillo","Audi",50,50,2023,"A1")
+#coche=Automovil("amarillo","Audi",50,50,2023,"A1")
 #print(f"El coche tiene {coche.ruedas} ruedas y una aceleración de {coche.aceleracion}")
 #Modificamos la acelaracion y mostrar en pantalla ambas acelaraciones
 #coche.aceleracion  = 20
@@ -16,14 +17,14 @@ coche=Automovil("amarillo","Audi",50,50,2023,"A1")
 #coche.volar()
 
 #creacion de un automovil 2
-coche2=Automovil("verde","Renault",10, 20,1969,"12")
+#coche2=Automovil("verde","Renault",10, 20,1969,"12")
 #coche2.acelera()
 #print (coche2.aceleracion)
 #coche.conducir()
 #coche.volar()
 
-coche3=Automovil("rojo","audi",20,60,2000,"r1")
-coche3.datos()
+#coche3=Automovil("rojo","audi",20,60,2000,"r1")
+#coche3.datos()
 #coche3.acelera()
 #print (coche3.aceleracion)
 #print (coche3.velocidad)
@@ -41,8 +42,8 @@ coche3.datos()
 
 #creacion de un automovil Volador
 
-cocheVolador=AutomovilVolador("gris","delorean",50, 50, 1970,"doc")
-cocheVolador.datos()
+#cocheVolador=AutomovilVolador("gris","delorean",50, 50, 1970,"doc")
+#cocheVolador.datos()
 
 #print (f"el automovil esta volando? {cocheVolador.esta_volando}")
 
@@ -55,8 +56,8 @@ cocheVolador.datos()
 
 
 #creo un vehiculo
-vehiculo1=Vehiculo("azul","chevrolet",10, 70, 2004,"corsa")
-vehiculo1.datos()
+#vehiculo1=Vehiculo("azul","chevrolet",10, 70, 2004,"corsa")
+#vehiculo1.datos()
 #print(vehiculo1._anio)
 #vehiculo1._anio=2022
 
@@ -67,3 +68,10 @@ vehiculo1.datos()
 #creo la suma
 #operacion.sumar(1,2)
 #print (f"la suma entre ambos numeros es {operacion.sumar}")
+
+#Sqlite3
+print("conecto a mi base de datos")
+conexion=sqlite3.connect("base_datos/mi_base.bd")
+cursor=conexion.cursor()
+cursor.execute('CREATE TABLE IF NOT EXISTS vehiculo(id INTEGER PRIMARY KEY AUTOINCREMENT,color TEXT, marca TEXT, aceleracion INTEGER,velocidad INTEGER, anio INTEGER,modelo TEXT)')
+conexion.commit()
